02c5cf8f1e7055d19a6552e362545e774f6162ea
irenaYo <irenatugushi997@gmail.com>
2026-04-03 20:57:50 +0300

    create UserService

---
+++ ResourceBooking/Service/Services/UserService.cs
@@ -0,0 +1,60 @@
+using Domain.Models;
+using Infrastructure.DTO;
+using Infrastructure.InterfacesRepositories;
+
+namespace Service.Services;
+
+public class UserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IHashService _hashService;
+
+    public UserService(IUserRepository userRepository, IHashService hashService)
+    {
+        _userRepository = userRepository;
+        _hashService = hashService;
+    }
+
+    public async Task<UserDto> CreateUser(CreateUserDto userDto)
+    {
+        if (await _userRepository.ExistsByEmail(userDto.Email))
+        {
+            throw new Exception("User already exists");
+        }
+        
+        string hashPassword = ConvertPassword(userDto.Password);
+        var role = Enum.Parse<UserRole>(userDto.Role, true);
+        
+        User user = new User(userDto.Name, userDto.Email, hashPassword, role);
+        await _userRepository.CreateUser(user);
+        
+        UserDto dto = ConvertUserToUserDto(user);
+        return dto;
+    }
+
+    public async Task<UserDto> Login(EnterUserDto enterUserDto)
+    {
+        User? user = await _userRepository.GetUserByEmail(enterUserDto.Email);
+        if (user == null)
+            throw new Exception("Invalid email");
+        
+        string hashPassword = ConvertPassword(enterUserDto.Password);
+        if (user.Password != hashPassword)
+            throw new Exception("The password is entered incorrectly");
+        
+        UserDto dto = ConvertUserToUserDto(user);
+        return dto;
+    }
+    
+    private string ConvertPassword(string password)
+    {
+        string hashPassword = _hashService.Hash(password);
+        return hashPassword;
+    }
+    
+    private UserDto ConvertUserToUserDto(User user)
+    {
+        UserDto dto = new UserDto(user.Id, user.Name, user.Email, user.Password, user.Role.ToString(), user.CreatedAt);
+        return dto;
+    }
+}
