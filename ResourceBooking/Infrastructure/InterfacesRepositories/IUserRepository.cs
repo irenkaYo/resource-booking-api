f12c22f31585ac7b3a6ddd4932d33496a84bb864
irenaYo <irenatugushi997@gmail.com>
2026-04-03 20:56:48 +0300

    replace by mistake

--- ResourceBooking/Infrastructure/InterfacesRepositories/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Models;
-
-namespace Infrastructure.InterfacesRepositories;
-
-public interface IUserRepository
-{
-    public Task CreateUser(User user);
-    public Task<User>? GetUserById(Guid userId);
-}
