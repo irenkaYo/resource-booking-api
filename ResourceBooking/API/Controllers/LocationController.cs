138fd4b4e7e342ea995a1712748046a6e7ad1699
irenaYo <irenatugushi997@gmail.com>
2026-04-08 09:04:26 +0300

    create LocationController

---
+++ ResourceBooking/API/Controllers/LocationController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Service.DTO.Location;
+using Service.Services;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("locations")]
+public class LocationController : ControllerBase
+{
+    private readonly LocationService _locationService;
+
+    public LocationController(LocationService locationService)
+    {
+        _locationService = locationService;   
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateLocation([FromBody] CreateLocationDto locationDto)
+    {
+        var location = await _locationService.CreateLocation(locationDto);
+        return CreatedAtAction(nameof(GetLocationById), new { id = location.Id }, location);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetLocationById(Guid id)
+    {
+        var location = await _locationService.GetLocationById(id);
+        return Ok(location);
+    }
+}
