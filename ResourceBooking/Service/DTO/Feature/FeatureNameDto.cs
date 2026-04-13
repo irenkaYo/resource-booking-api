3e2592d1c1b8e08e5a5b03601b5616322fbd85b9
irenaYo <irenatugushi997@gmail.com>
2026-04-13 20:17:51 +0300

    add feature dto

---
+++ ResourceBooking/Service/DTO/Feature/FeatureNameDto.cs
@@ -0,0 +1,11 @@
+namespace Service.DTO.Feature;
+
+public class FeatureNameDto
+{
+    public string Name { get; set; }
+
+    public FeatureNameDto(string name)
+    {
+        Name = name;
+    }
+}
