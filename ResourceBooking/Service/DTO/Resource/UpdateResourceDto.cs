91cd15dc22844a1817ea8813fc405d3bcb60a9e0
irenaYo <irenatugushi997@gmail.com>
2026-04-14 21:21:36 +0300

    update UpdateResourceDto

--- ResourceBooking/Service/DTO/Resource/UpdateResourceDto.cs
+++ ResourceBooking/Service/DTO/Resource/UpdateResourceDto.cs
@@ -2,10 +2,10 @@ namespace Infrastructure.DTO.Resource;
 
 public class UpdateResourceDto
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public Guid LocationId { get; set; }
-    public Guid CategoryId { get; set; }
-    public int Capacity { get; set; }
-    public string RowVersion { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public Guid? LocationId { get; set; }
+    public Guid? CategoryId { get; set; }
+    public int? Capacity { get; set; }
+    public uint xmin { get; set; }
 }
